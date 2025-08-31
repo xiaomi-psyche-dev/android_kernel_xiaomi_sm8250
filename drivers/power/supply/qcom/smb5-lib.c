d69c3f830f9124d06f5147048e2792a8458ea9d6
Rocky7842 <eric.rocky7842@gmail.com>
2025-08-31 23:13:44 +0800

    power: qpnp-smb5: Remove battery_charging_enabled sysfs node
    * This is broken, and we already have working charging_enabled node
      modified from night_charging.
    
    Change-Id: Iafcbdf6fe00a461b9a02b91c8c56ebd212faa34b

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -2545,29 +2545,6 @@ static bool is_charging_paused(struct smb_charger *chg)
 	return val & CHARGING_PAUSE_CMD_BIT;
 }
 
-int smblib_get_prop_battery_charging_enabled(struct smb_charger *chg,
-					union power_supply_propval *val)
-{
-	int icl = 0;
-	if (chg->is_qc_class_a && !chg->qc3_raise_done)
-		icl = MAIN_ICL_MIN;
-
-	if (chg->six_pin_step_charge_enable) {
-		val->intval = !(get_client_vote(chg->usb_icl_votable, MAIN_ICL_MIN_VOTER)
-				== MAIN_ICL_MIN);
-	}
-	else {
-		if (chg->power_good_en)
-			val->intval = !(get_client_vote(chg->usb_icl_votable, MAIN_CHG_SUSPEND_VOTER)
-					== MAIN_CHG_SUSPEND_ICL);
-		else
-			val->intval = !(get_client_vote(chg->usb_icl_votable, MAIN_CHG_SUSPEND_VOTER)
-					== icl);
-	}
-	return 0;
-}
-
-
 int smblib_get_prop_batt_status(struct smb_charger *chg,
 				union power_supply_propval *val)
 {
@@ -3639,49 +3616,6 @@ int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 		schedule_delayed_work(&chg->thermal_setting_work, 3 * HZ);
 	else
 		smblib_therm_charging(chg);
-
-	return 0;
-}
-
-int smblib_set_prop_battery_charging_enabled(struct smb_charger *chg,
-				const union power_supply_propval *val)
-{
-	int icl = 0;
-	if (chg->is_qc_class_a && !chg->qc3_raise_done)
-		icl = MAIN_ICL_MIN;
-
-	if (val->intval == 0) {
-		if (chg->six_pin_step_charge_enable) {
-			vote(chg->usb_icl_votable, MAIN_ICL_MIN_VOTER,
-						true, MAIN_ICL_MIN);
-		}
-		else {
-			if (chg->power_good_en)
-				vote(chg->usb_icl_votable, MAIN_CHG_SUSPEND_VOTER,
-						true, MAIN_CHG_SUSPEND_ICL);
-			else
-				vote(chg->usb_icl_votable, MAIN_CHG_SUSPEND_VOTER,
-						true, icl);
-		}
-#if (!defined CONFIG_FUEL_GAUGE_BQ27Z561) && (!defined CONFIG_DUAL_FUEL_GAUGE_BQ27Z561)
-		schedule_delayed_work(&chg->reduce_fcc_work,
-			msecs_to_jiffies(ESR_WORK_TIME_97S));
-#endif
-
-
-	} else {
-		if (chg->six_pin_step_charge_enable)
-			vote(chg->usb_icl_votable, MAIN_ICL_MIN_VOTER,
-						false, 0);
-		else
-			vote(chg->usb_icl_votable, MAIN_CHG_SUSPEND_VOTER,
-						false, 0);
-#if (!defined CONFIG_FUEL_GAUGE_BQ27Z561) && (!defined CONFIG_DUAL_FUEL_GAUGE_BQ27Z561)
-		if (is_client_vote_enabled(chg->fcc_votable, ESR_WORK_VOTER))
-			vote(chg->fcc_votable, ESR_WORK_VOTER, false, 0);
-		cancel_delayed_work_sync(&chg->reduce_fcc_work);
-#endif
-	}
 
 	return 0;
 }
